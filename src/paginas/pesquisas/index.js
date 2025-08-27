3d72af1eb55fa7a5d80d4d87215540d64e6c2cc5
Your Name <you@example.com>
2025-08-27 14:19:29 -0300

    apifeita

--- src/paginas/pesquisas/index.js
+++ src/paginas/pesquisas/index.js
@@ -1,53 +1,95 @@
 import { useRoute } from '@react-navigation/native';
-import { View ,Text, Image, FlatList } from 'react-native';
-import { useEffect,useState } from 'react';
-export default function PesquisasFilmes(){
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
 
-   const route = useRoute();
-   const[filmes,setFilmes] = useState([''])
+export default function PesquisasFilmes() {
+  const route = useRoute();
+  const [filmes, setFilmes] = useState([]);
 
-   useEffect(()=>{
+  useEffect(() => {
+    async function buscarFilmes() {
+      const url = `https://api.themoviedb.org/3/search/movie?query=${route.params.Pesquisa}&include_adult=false&language=pt-BR&page=1`;
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzY1YzEyYzUyMWRiMTEzMjJmZDk1MDJlMWNlYzlmYyIsIm5iZiI6MTc1NTAyMTUxMi40NzksInN1YiI6IjY4OWI4MGM4MzVlMGUyZDIxMDZkNzRiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EAMFVwLizTgWASktiIgkzwldJO4_he776g98_jlTAY8',
+        },
+      };
 
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setFilmes(data.results);
+    }
 
-async function buscarFilmes(){
-    const url = `https://api.themoviedb.org/3/search/movie?query=${route.params.Pesquisa}s&include_adult=false&language=pt-BR&page=1`
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzY1YzEyYzUyMWRiMTEzMjJmZDk1MDJlMWNlYzlmYyIsIm5iZiI6MTc1NTAyMTUxMi40NzksInN1YiI6IjY4OWI4MGM4MzVlMGUyZDIxMDZkNzRiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EAMFVwLizTgWASktiIgkzwldJO4_he776g98_jlTAY8'
-  }
-   
+    buscarFilmes();
+  }, []);
 
-};
-
-const response = await fetch(url,options)
-const data = await response.json()
-console.log(data.results)
-setFilmes(data.results)
-
+  return (
+    <View style={styles.container}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={filmes}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.cardContainer}>
+            <Image
+              style={styles.image}
+              source={{ uri: `https://image.tmdb.org/t/p/original${item.poster_path}` }}
+            />
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.vote}>Nota: {item.vote_average}</Text>
+            <Text style={styles.sinopse}>{item.overview}</Text>
+          </View>
+        )}
+        contentContainerStyle={styles.flatlistContent}
+      />
+    </View>
+  );
 }
 
-buscarFilmes();
-
-   },[])
-
-    
-   
-   return (
-       <View>
-           <FlatList
-           data={filmes}
-           keyExtractor={(item)=>item.id}
-           renderItem={({item})=>(
-               <View>
-                   <Text> filme:{item.title} </Text>
-                   <Text>nota :{item.vote_average}</Text>
-                   <Image style = {{width:"100%", height:150}} source={{uri:(`https://image.tmdb.org/t/p/original${item.poster_path}`)}}></Image>
-               </View>
-           )}>
-           </FlatList>
-          
-       </View>
-   )
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 10,
+  },
+  flatlistContent: {
+    paddingBottom: 20,
+  },
+  cardContainer: {
+    backgroundColor: '#1c1c1c',
+    marginBottom: 15,
+    borderRadius: 10,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  vote: {
+    color: '#ccc',
+    fontSize: 14,
+    marginTop: 5,
+    textAlign: 'center',
+  },
+  sinopse: {
+    color: '#aaa',
+    fontSize: 12,
+    marginTop: 5,
+    textAlign: 'center',
+  },
+});
--- src/rotas/index.js
+++ src/rotas/index.js
@@ -4,22 +4,34 @@ import Home from "../paginas/home";
 import Detalhes from "../paginas/detalhes";
 import PesquisasFilmes from "../paginas/pesquisas";
 
-
-export default function Rotas(){
-
+export default function Rotas() {
     const stack = createNativeStackNavigator();
-    return(
+    return (
         <NavigationContainer>
-
-            <stack.Navigator screenOptions={{headerShown:false}}>
-                <stack.Screen name="Home" component={Home}/>
-                <stack.Screen name="Detalhes" component={Detalhes} options={{headerShown:true}}/>
-                < stack.Screen name='pesquisa' component={PesquisasFilmes}
-                options={{
-                    headerShown:true
-                }} ></stack.Screen>
+            <stack.Navigator screenOptions={{ headerShown: false }}>
+                <stack.Screen 
+                    name="Home" 
+                    component={Home} 
+                />
+                <stack.Screen 
+                    name="Detalhes" 
+                    component={Detalhes} 
+                    options={{
+                        headerShown: true,
+                        headerStyle: { backgroundColor: 'black' }, 
+                        headerTintColor: 'white', 
+                    }} 
+                />
+                <stack.Screen 
+                    name="pesquisa" 
+                    component={PesquisasFilmes} 
+                    options={{
+                        headerShown: true,
+                        headerStyle: { backgroundColor: 'black' }, 
+                        headerTintColor: 'white', 
+                    }} 
+                />
             </stack.Navigator>
-            
         </NavigationContainer>
-    )
+    );
 }
